c2095eaa304ced64d86c551cd8cf19677154834e
JulianoDM <julianodiasmchado@gmail.com>
2023-10-02 23:03:26 -0300

    subindo codigo de desenvolvimento

--- inventarioServer/server.js
+++ inventarioServer/server.js
@@ -4,6 +4,6 @@ import app from "./src/app.js";
 const port = 3000;
 
 app.listen(port, () =>{
-    console.log(`Escutando na porta http://localhost:${port}`);
+    console.log(`Escutando na porta: http://localhost:${port}`);
 });
 
